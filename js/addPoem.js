3a2288f3b4be1cc84092af33752b43fcd100bea1
unknown <tomerpacific@gmail.com>
2020-10-09 10:07:11 +0300

    feature/poem-writing-age adding buttons in modal to take user to login or signup

--- js/addPoem.js
+++ js/addPoem.js
@@ -1,12 +1,27 @@
 window.onload = () => {
+
     if (!isUserLoggedIn()) {
         disableUI();
+        attachListenersToButtons();
         presentDialog();
     }
 }
 
 function isUserLoggedIn() {
     return localStorage.getItem('tk') !== null;
+}
+
+function attachListenersToButtons() {
+    var loginBtn = document.querySelector("#loginBtn");
+    var signupBtn = document.querySelector("#signupBtn");
+
+    loginBtn.onclick = function() {
+        location.href = 'login.html';
+    }
+
+    signupBtn.onclick = function() {
+        location.href = 'signup.html';
+    }
 }
 
 function disableUI() {
