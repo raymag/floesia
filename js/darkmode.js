cebd90382539976018211b4833530115f692056e
raymag <prayk11@outlook.com>
2020-10-10 19:31:28 -0300

    feat: remove onload from darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,38 +1,35 @@
 
-window.onload = () => {
-    applyDefaultTheme();
 
-    const darkmodeToggler = document.querySelectorAll(".darkmode-toggler");
-    darkmodeToggler.forEach(el => el.addEventListener('click', () => {
-        toggleMode();
-    }));
+applyDefaultTheme();
+const darkmodeToggler = document.querySelectorAll(".darkmode-toggler");
+darkmodeToggler.forEach(el => el.addEventListener('click', () => {
+    toggleMode();
+}));
+function applyDefaultTheme(){
+    const darkmode = localStorage.getItem("dk");
+    console.log(darkmode);
+    if (darkmode) {
+        applyDarkMode();
+    } else {
+        applyLightMode();
+    }
+}
 
-    function applyDefaultTheme(){
-        const darkmode = localStorage.getItem("dk");
-        console.log(darkmode);
-        if (darkmode) {
-            applyDarkMode();
-        } else {
-            applyLightMode();
-        }
-    }
-    
-    function applyDarkMode(){
-        document.body.classList.add("darkmode");
-    }
-    
-    function applyLightMode(){
-        document.body.classList.remove("darkmode");
-    }
-    
-    function toggleMode(){
-        const darkmode = localStorage.getItem("dk");
-        if (darkmode) {
-            localStorage.removeItem("dk");
-            applyLightMode();
-        } else {
-            localStorage.setItem("dk", true);
-            applyDarkMode();
-        }
+function applyDarkMode(){
+    document.body.classList.add("darkmode");
+}
+
+function applyLightMode(){
+    document.body.classList.remove("darkmode");
+}
+
+function toggleMode(){
+    const darkmode = localStorage.getItem("dk");
+    if (darkmode) {
+        localStorage.removeItem("dk");
+        applyLightMode();
+    } else {
+        localStorage.setItem("dk", true);
+        applyDarkMode();
     }
 }
